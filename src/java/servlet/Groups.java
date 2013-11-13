c287a12c64f3a3774bd6790cfa8a8e6144dda0ad
Kairos90 <freip@hotmail.it>
2013-11-13 15:32:57 +0100

    Modified

--- src/java/servlet/Groups.java
+++ src/java/servlet/Groups.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package servlet;
 
+import db.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -18,12 +18,92 @@ import javax.servlet.http.HttpServletResponse;
  * @author Pier DAgostino
  */
 public class Groups extends HttpServlet {
-    
+
     // TITLE OF GROUPS PAGE & CONTENT OF GROUPS PAGE
-    private static String GROUPS_TITLE = "Groups";
-    private static String GROUPS_CONTENT = ""
-            + "<div>"
-            + "";
+    private static String GROUPS_TITLE = "My Groups";
+    private static String GROUPS_CONTENT = "<table data-role=\"table\" id=\"table-custom-2\" data-mode=\"columntoggle\" class=\"ui-body-d ui-shadow table-stripe ui-responsive\" data-column-btn-theme=\"b\" data-column-btn-text=\"Columns to display...\" data-column-popup-theme=\"a\">\n" +
+"         <thead>\n" +
+"           <tr class=\"ui-bar-d\">\n" +
+"             <th data-priority=\"2\">Group Name</th>\n" +
+"             <th>News</th>\n" +
+"             <th data-priority=\"3\">Latest Post</th>\n" +
+"             <th data-priority=\"1\"><abbr title=\"Rotten Tomato Rating\">Modifica</abbr></th>\n" +
+"             <th data-priority=\"5\">Resoconto</th>\n" +
+"           </tr>\n" +
+"         </thead>\n" +
+"         <tbody>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"           <tr>\n" +
+"             <th></th>\n" +
+"             <td class=\"title\"><a href=\"\" data-rel=\"external\"></a></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"             <td></td>\n" +
+"           </tr>\n" +
+"         </tbody>\n" +
+"       </table>";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -37,8 +117,12 @@ public class Groups extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        //Getting user informations
+        //User requesting = (User) request.getSession().getAttribute("user");
+        
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            
             HTML.printPage(out, GROUPS_TITLE, GROUPS_CONTENT);
         }
     }
