fb3c280e1cc988e8c2c8786dd8a656fe79228e65
halfblood17 <halfblood17@gmail.com>
2013-11-16 19:34:48 +0100

    bugfix

--- src/java/servlet/Home.java
+++ src/java/servlet/Home.java
@@ -27,7 +27,7 @@ public class Home extends HttpServlet {
             + "<li><a href=\"forum/groups\">My groups</a></li>"
             + "<li><a href=\"forum/create\">Create group</a></li>"
             + "<li><a href=\"forum/invites\">Invites</a></li>"
-            + "<li><a href=\"/logout\">Logout</a></li>"
+            + "<li><a href=\"/logout\" data-ajax=\"false\">Logout</a></li>"
             + "</ul>";
 
     /**
