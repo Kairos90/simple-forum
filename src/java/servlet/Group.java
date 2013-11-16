00b8064c352d8d0d22ace32da64d95fed3795492
Kairos90 <freip@hotmail.it>
2013-11-16 16:15:15 +0100

    changed GroupX to Group

--- src/java/servlet/Group.java
+++ src/java/servlet/Group.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Pier DAgostino
  */
-public class GroupX extends HttpServlet {
+public class Group extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
