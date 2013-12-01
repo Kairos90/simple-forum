19adf4a1ea50c5ced6c8809aa07ec48f86c12830
m4ff <maffonline@live.com>
2013-12-01 18:36:47 +0100

    ddddd

--- web/static/utils.js
+++ web/static/utils.js
@@ -1,6 +1,9 @@
 function duplicate(selector) {
     var e = $(selector);
-    e.clone().insertAfter(e).show(500);
+    var c = e.clone();
+    var i = c.find("input");
+    i.attr("name", i.attr("name") + "1");
+    c.insertAfter(e).show();
     return true;
 }
 
