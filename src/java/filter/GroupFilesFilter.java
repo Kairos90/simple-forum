01b3decfc7ccf8d33aecdc32fa0dcb2db5d14ba7
m4ff <maffonline@live.com>
2013-12-02 18:54:07 +0100

    qwertyuioop

--- src/java/filter/GroupFilesFilter.java
+++ src/java/filter/GroupFilesFilter.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package filter;
 
 import db.DBManager;
@@ -23,7 +22,7 @@ import javax.servlet.http.HttpSession;
  *
  * @author paolo
  */
-public class GroupFilesFilter implements Filter{
+public class GroupFilesFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -33,26 +32,27 @@ public class GroupFilesFilter implements Filter{
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpSession session = ((HttpServletRequest) request).getSession();
         User user = (User) session.getAttribute("user");
-        DBManager manager = (DBManager) request.getServletContext().getAttribute("dbmanager");
-        HttpServletRequest req = (HttpServletRequest) request;
-        //String requestUri = req.getRequestURI().substring(14, group);
-        //groupId = groupId.substring(0, groupId.indexOf('/'));
-        //System.out.println(groupId);
-        String id = request.getParameter("id");
-        if (id != null && !"0".equals(id)) {
-            int idParam = Integer.parseInt(id);
-            if (manager.checkIfUserCanAccessGroup(user.getId(), idParam)) {
-                ((HttpServletResponse) response).sendRedirect("/forum");
-            } else {
-                chain.doFilter(request, response);
+        if (user != null) {
+            DBManager manager = (DBManager) request.getServletContext().getAttribute("dbmanager");
+            HttpServletRequest req = (HttpServletRequest) request;
+            String requestUri = req.getRequestURI();
+            try {
+                int groupId = Integer.parseInt(requestUri.substring(0, requestUri.indexOf('/')));
+                if (manager.checkIfUserCanAccessGroup(user.getId(), groupId)) {
+                    ((HttpServletResponse) response).sendRedirect("/forum");
+                } else {
+                    chain.doFilter(request, response);
+                }
+                return;
+            } catch (Exception e) {
+                
             }
-        } else {
-            chain.doFilter(request, response);
         }
+        ((HttpServletResponse) response).sendRedirect("/forum");
     }
 
     @Override
     public void destroy() {
     }
-    
+
 }
